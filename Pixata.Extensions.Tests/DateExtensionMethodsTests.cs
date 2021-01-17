08fa4cdd49e94070d120680ccc52b8616e3689d3
Avrohom Yisroel Silver <mryossu@hotmail.com>
2021-01-17 16:33:55 +0000

    Added an extra test to DateExtensionMethodsTests, mainly to if the new build worflow works

--- Pixata.Extensions.Tests/DateExtensionMethodsTests.cs
+++ Pixata.Extensions.Tests/DateExtensionMethodsTests.cs
@@ -5,9 +5,15 @@ namespace Pixata.Extensions.Tests {
   [TestClass]
   public class DateExtensionMethodsTests {
     [TestMethod]
-    public void DateExtensionMethods_Test() {
-      DateTime d = new(2021,1,14);
-      Assert.AreEqual("14th January 2021",d.ToPrettyString());
+    public void DateExtensionMethods_14thJan2021() {
+      DateTime d = new(2021, 1, 14);
+      Assert.AreEqual("14th January 2021", d.ToPrettyString());
+    }
+
+    [TestMethod]
+    public void DateExtensionMethods_31stDec2021() {
+      DateTime d = new(2021, 12, 31);
+      Assert.AreEqual("31st December 2021", d.ToPrettyString());
     }
   }
 }
