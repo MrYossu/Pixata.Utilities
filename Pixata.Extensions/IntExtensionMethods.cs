21eb296660420085ad634bb38cc86881d7a522fa
Avrohom Yisroel Silver <mryossu@hotmail.com>
2021-01-19 22:20:33 +0000

    Updated IntExtensionMethods.OrdinalSuffix() as the master version was behind the release (how? No idea)

--- Pixata.Extensions/IntExtensionMethods.cs
+++ Pixata.Extensions/IntExtensionMethods.cs
@@ -3,10 +3,11 @@
     /// <summary>
     /// Returns the ordinal suffix for a number, eg "st" for 1, "nd" for 2, "rd" for 3 and so on. Useful for formatting dates
     /// </summary>
-    /// <param name="n"></param>
-    /// <returns></returns>
-    public static string OrdinalSuffix(this int n) =>
-      n switch {
+    /// <param name="n">An integer</param>
+    /// <param name="includeNumber">If true (9)default) then includes the number in the return value, eg "9th". If false, returns just the ordinal suffix, eg "th"</param>
+    /// <returns>The ordinal suffix for the integer, optionally with the integer itself</returns>
+    public static string OrdinalSuffix(this int n, bool includeNumber = true) =>
+      (includeNumber ? n : "") + n switch {
         11 or 12 or 13 => "th",
         _ when n.ToString().EndsWith("1") => "st",
         _ when n.ToString().EndsWith("2") => "nd",
