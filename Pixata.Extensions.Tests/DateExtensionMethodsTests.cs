d63675da7116c118b9c16c0619107f7e1eab1b90
Avrohom Yisroel Silver <mryossu@hotmail.com>
2021-01-19 20:20:19 +0000

    Renamed three unit tests

--- Pixata.Extensions.Tests/DateExtensionMethodsTests.cs
+++ Pixata.Extensions.Tests/DateExtensionMethodsTests.cs
@@ -7,19 +7,19 @@ namespace Pixata.Extensions.Tests {
     #region ToPrettyString
 
     [TestMethod]
-    public void DateExtensionMethods_14thJan2021() {
+    public void DateExtensionMethods_ToPrettyString_14thJan2021() {
       DateTime d = new(2021, 1, 14);
       Assert.AreEqual("14th January 2021", d.ToPrettyString());
     }
 
     [TestMethod]
-    public void DateExtensionMethods_7thApril1961() {
+    public void DateExtensionMethods_ToPrettyString_7thApril1961() {
       DateTime d = new(1961, 4, 7);
       Assert.AreEqual("7th April 1961", d.ToPrettyString());
     }
 
     [TestMethod]
-    public void DateExtensionMethods_31stDec2021() {
+    public void DateExtensionMethods_ToPrettyString_31stDec2021() {
       DateTime d = new(2021, 12, 31);
       Assert.AreEqual("31st December 2021", d.ToPrettyString());
     }
