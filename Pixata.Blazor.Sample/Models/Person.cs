454fb917f6b370d5b832f701e7c7f682f1a5d5b5
Avrohom Yisroel Silver <mryossu@hotmail.com>
2021-05-27 17:04:10 +0100

    Added FormRowTimeTelerik to Pixata.Blazor.TelerikComponents, added a form sample page to the sample project and did a few minor tweaks to other Blazor components

---
+++ Pixata.Blazor.Sample/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pixata.Blazor.Sample.Models {
+  public class Person {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string Surname { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public DateTime TeaTime { get; set; }
+    public bool EmailConfirmed { get; set; }
+    public int NumberOfDependents { get; set; }
+    public string Notes { get; set; }
+  }
+}
---
+++ Pixata.Blazor.Sample/Validators/PersonValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FluentValidation;
+using Pixata.Blazor.Sample.Models;
+
+namespace Pixata.Blazor.Sample.Validators {
+  public class PersonValidator : AbstractValidator<Person> {
+    public PersonValidator() {
+      RuleFor(p => p.FirstName).NotEmpty().WithMessage("Required");
+      RuleFor(p => p.Surname).NotEmpty().WithMessage("Required");
+      RuleFor(p => p.DateOfBirth).GreaterThan(new DateTime(2000, 1, 1)).WithMessage("After 1st Jan 2000");
+    }
+  }
+}
