fa6b4d6801753e58083f8046cb2b6ee30f12a170
Avrohom Yisroel Silver <mryossu@hotmail.com>
2021-11-17 16:56:55 +0000

    Added the Pixata.Google project

---
+++ Pixata.Google/GoogleDriveHelper.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using LanguageExt;
+using Microsoft.AspNetCore.Hosting;
+using static LanguageExt.Prelude;
+using DriveFile = Google.Apis.Drive.v3.Data.File;
+
+namespace Pixata.Google {
+  public class GoogleDriveHelper {
+    #region Private variables and constructor
+
+    public const string RootFolderName = "My Drive";
+
+    private readonly DriveService _service;
+
+    public GoogleDriveHelper(IWebHostEnvironment environment) =>
+      _service = GetDriveService(environment);
+
+    #endregion
+
+    #region Folders
+
+    /// <summary>
+    /// Gets the folder with the specified Id
+    /// </summary>
+    /// <param name="folderId"></param>
+    /// <returns>The specified folder</returns>
+    public TryAsync<DriveFile> GetFolder(string folderId = "root") =>
+      TryAsync(() => _service.Files.Get(folderId).ExecuteAsync());
+
+    /// <summary>
+    /// Gets the parent folder of the one whose Id is passed in
+    /// </summary>
+    /// <param name="folderId">The Id of the folder whose parent is required</param>
+    /// <returns>The DriveFile object representing the parent folder</returns>
+    public TryAsync<DriveFile> GetParentFolder(string folderId) =>
+      TryAsync(() => GetParentFolderDo(folderId));
+
+    private async Task<DriveFile> GetParentFolderDo(string folderId) {
+      FilesResource.GetRequest request = _service.Files.Get(folderId);
+      request.Fields = "id, name, parents";
+      DriveFile folder = await request.ExecuteAsync();
+      request = _service.Files.Get(folder.Parents[0]);
+      return await request.ExecuteAsync();
+    }
+
+    /// <summary>
+    /// Returns a list of the subfolders of the folder who Id is passed in
+    /// </summary>
+    /// <param name="folderId">The Id of the folder in Google Drive</param>
+    /// <returns>A list of the subfolders of the folder who Id is passed in</returns>
+    public TryAsync<List<DriveFile>> GetSubfolders(string folderId = "root") {
+      FilesResource.ListRequest request = _service.Files.List();
+      request.Q = $"mimeType = 'application/vnd.google-apps.folder' and '{folderId}' in parents and trashed = false";
+      return TryAsync(async () => (await request.ExecuteAsync()).Files.OrderBy(f => f.Name).ToList());
+    }
+
+    /// <summary>
+    /// Creates a new folder as a subfolder of the specified one
+    /// </summary>
+    /// <param name="folderName">The name for the new folder</param>
+    /// <param name="parentFolderId">The Id of the parent folder. If omitted, the new folder will be created in the root</param>
+    /// <returns>The Id of the new folder</returns>
+    // TODO AYS - Convert to use TryAsync
+    public async Task<Either<Exception, string>> CreateFolder(string folderName, string parentFolderId = "root") =>
+      await (await GetSubfolders(parentFolderId))
+        .Match(async folders => await CreateNewFolder(folderName, parentFolderId, folders), ex => throw ex);
+
+    // Gives compiler error - call is ambiguous
+    //public TryAsync<string> CreateFolder2(string folderName, string parentFolderId = "root") =>
+    //  GetSubfolders(parentFolderId).Match(folders => CreateNewFolder(folderName, parentFolderId, folders), ex => throw ex);
+
+    private async Task<string> CreateNewFolder(string folderName, string parentFolderId, List<DriveFile> folders) {
+      if (folders.Any()) {
+        return folders.First().Id;
+      }
+      DriveFile newFolder = new() {
+        Name = folderName,
+        MimeType = "application/vnd.google-apps.folder",
+        Parents = new[] { parentFolderId }
+      };
+      FilesResource.CreateRequest command = _service.Files.Create(newFolder);
+      DriveFile folder = await command.ExecuteAsync();
+      return folder.Id;
+    }
+
+    #endregion
+
+    #region Files
+
+    /// <summary>
+    /// Returns the files in the specified folder
+    /// </summary>
+    /// <param name="folderId">The Id of the specific folder</param>
+    /// <returns>The files in the specified folder</returns>
+    public TryAsync<List<DriveFile>> GetFilesInFolder(string folderId = "root") =>
+      TryAsync(async () => {
+        FilesResource.ListRequest fileList = _service.Files.List();
+        fileList.Q = $"mimeType != 'application/vnd.google-apps.folder' and '{folderId}' in parents and trashed = false";
+        fileList.Fields = "nextPageToken, files(id, name, size, mimeType)";
+        List<DriveFile> files = new();
+        string pageToken = null;
+        do {
+          fileList.PageToken = pageToken;
+          FileList filesResult = await fileList.ExecuteAsync();
+          IList<DriveFile> pageFiles = filesResult.Files;
+          pageToken = filesResult.NextPageToken;
+          files.AddRange(pageFiles);
+        } while (pageToken != null);
+        return files.OrderBy(f => f.Name).ToList();
+      });
+
+    /// <summary>
+    /// Uploads a file to the specified folder
+    /// </summary>
+    /// <param name="file">A Stream containing the file data</param>
+    /// <param name="fileName">The file name</param>
+    /// <param name="mimeType">The MIME type of the file</param>
+    /// <param name="folderId">The parent folder Id</param>
+    /// <returns>The Id of the uploaded file</returns>
+    public TryAsync<string> UploadFile(Stream file, string fileName, string mimeType, string folderId) =>
+      TryAsync(async () => {
+        DriveFile driveFile = new() {
+          Name = fileName,
+          MimeType = mimeType,
+          Parents = new[] { folderId }
+        };
+        FilesResource.CreateMediaUpload request = _service.Files.Create(driveFile, file, mimeType);
+        request.Fields = "id";
+        IUploadProgress response = await request.UploadAsync();
+        if (response.Status != UploadStatus.Completed) {
+          throw response.Exception;
+        }
+        return request.ResponseBody.Id;
+      });
+
+    /// <summary>
+    /// Deletes a file
+    /// </summary>
+    /// <param name="fileId">The Id of the file to be deleted</param>
+    /// <returns>An empty string if the deletion succeeded. Not sure if this can ever return a non-empty string, as if the deletion was not successful, it's most likely an exception occurred</returns>
+    public TryAsync<string> DeleteFile(string fileId) =>
+      TryAsync(() => _service.Files.Delete(fileId).ExecuteAsync());
+
+    #endregion
+
+    #region Utility
+
+    private DriveService GetDriveService(IWebHostEnvironment environment) {
+      UserCredential credential;
+      using (FileStream stream = new(@$"{environment.ContentRootPath}\Google\credentials.json", FileMode.Open, FileAccess.Read)) {
+        // If you modify the scopes, make sure to delete your previously saved credentials so new ones can be created
+        credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.FromStream(stream).Secrets, new[] { DriveService.Scope.Drive }, "user", CancellationToken.None, new FileDataStore(@$"{environment.ContentRootPath}\Google\Token", true)).Result;
+      }
+      DriveService service = new(new BaseClientService.Initializer {
+        HttpClientInitializer = credential,
+        ApplicationName = ""
+      });
+      return service;
+    }
+
+    #endregion
+  }
+}
---
+++ Pixata.Google/README.md
@@ -0,0 +1,65 @@
+# Pixata.Google [![Release status](https://github.com/MrYossu/Pixata.Google/workflows/release/badge.svg)](https://github.com/MrYossu/Pixata.Google/actions?query=workflow%3Arelease) [![Master status](https://github.com/MrYossu/Pixata.Google/workflows/master/badge.svg)](https://github.com/MrYossu/Pixata.Google/actions?query=workflow%3Amaster) ![Last commit to master](https://img.shields.io/github/last-commit/MrYossu/Pixata.Google/master)
+
+![Pixata](https://github.com/MrYossu/Pixata.Google/raw/master/PixataGoogle.png "Pixata")
+
+This project contains utility code for working with Google's APIs. At the moment, it onl contains one class, but I'll refer to "classes" (in th eplural) below, as I will hopefully add more as time goes on.
+
+A [Nuget package](https://www.nuget.org/packages/Pixata.Blazor.LanguageExtComponents/) is available for this project.
+
+## Setting up Google authentication
+
+This class was designed to be used within an ASP.NET Core application, and allows easy access to a Google Drive account.
+
+In order to use this class, you need to follow a small convention. I would like to be able to relax this, but haven't found a way yet, so bear with me...
+
+You will need to create a credentials file for your Google account. For details, see the [.NET Quickstart](https://developers.google.com/drive/api/v3/quickstart/dotnet), and follow the first few steps. This will give you a JSON file, which you will need to save as `credentials.json` in a top-level folder named `Google`...
+
+![Pixata](https://github.com/MrYossu/Pixata.Utilities/raw/master/Pixata.Google/Icon/GoogleFolder.png "A false match") 
+
+When you first run the application, you will be prompted to autheticate. This will create a folder named `Token` in the `Google` folder (see the image above), which will contain a JSON file with the details needed for your application to access Google Drive with the account you used when authenticating.
+
+Note that you will need to edit `credentials.json` to include the URL for the application. You may need to add two entries, one with and one without a trailing slash...
+
+```json
+"redirect_uris": [
+  "https://localhost:44365/authorize/",
+  "https://localhost:44365/authorize"
+],
+```
+
+Make sure that the port number shown in the file matches the one you see in your browser when running the site. When you come to deploy your application, don't forget to add the URIs for the deployed site. If you get a Google error saying the URI doesn't match, ignore the one they suggest as it is almost always wrong! Double-check the URI in your browser and use that.
+
+## LanguageExt
+The classes here are based on the rather excellent [LanguageExt](https://github.com/louthy/language-ext/) Nuget package. What this means for you is that you will need to adopt a functional approach to using the methods in these classes. This gives a much more robust code base than would have been possible without.
+
+In general, most methods return a `TryAsync<T>`, where `T` is the type of the value you want returned. This can be consumed with code like the following. Suppose you have a `List<File>` (where `File` is the Google Drive API type for a file or folder) named `Folders` and a `string` variable named `Msg` which is used to display messages to the user. You could then get the subfolders of a specified folder as follows...
+
+```c#
+    private async Task LoadSubfolders() =>
+      (await GoogleDriveHelper.GetSubfolders(CurrentFolder.Id))
+        .Match(
+          folders => Folders = folders,
+          ex => Msg = $"Ex: {ex.Message}");
+```
+
+The `Match` method takes two lambdas, one that is called when the operation succeeds (in which case we copy the folder list to `Folders`) and one that is called in case something went wrong with the call. In that case, an `Exception` is passed. In the sample, we just inform the user that an exception occurred.
+
+If you are going to set the `Msg` variable in either case, then there is a simpler format. For example, the API call to create a folder returns the `Id` of the newly-created folder. In that case, informing the user of the result could be done like this...
+
+```c#
+private async Task CreateFolder(MouseEventArgs arg) =>
+  Msg += (await GoogleDriveHelper.CreateFolder(_newFolderId, CurrentFolder.Id))
+    .Match(id => $"New folder Id: {id}", ex => $"Ex: {ex.Message}");
+```
+
+Note that as both lambdas passed to `Match` return the same type (a `string`), we can move the assignment (`Msg += `) outside the call to `Match`.
+
+If this isn't clear, then I very strongly recommend you read [Functional Programming in C#](https://www.manning.com/books/functional-programming-in-c-sharp?query=functional%20programming%20c#), which is one of the best C# books I've read (and re-read, and re-read...) for a long time. Once you are familiar with the concepts, the above will be much clearer.
+
+## The classes
+### GoogleDriveHelper
+This allows easy access to a Google Drive account. You will need to inject an instance of the class into your code.
+
+If you look in the source code, you can see the methods. They all have XML comments, so you should get hints when you use them as well.
+
+Note that the API expects file and folder `Id`s, **not** names. This is easy to forget at first, and will result in errors.
