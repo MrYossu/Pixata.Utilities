553725d8ae7eaa5deffe7e8cfc53e70376ef20e4
Avrohom Yisroel Silver <mryossu@hotmail.com>
2022-02-01 16:06:37 +0000

    Updated all the form row controls so they show a red border on validation error (needs CSS adding). Updated the sample project to show this

--- Pixata.Blazor.Sample/Models/Person.cs
+++ Pixata.Blazor.Sample/Models/Person.cs
@@ -4,20 +4,10 @@ using System.ComponentModel.DataAnnotations;
 namespace Pixata.Blazor.Sample.Models {
   public class Person {
     public int Id { get; set; }
-
-    [Required(ErrorMessage = "required")]
     public string Title { get; set; }
-
-    [Required(ErrorMessage = "required")]
     public string FirstName { get; set; }
-
-    [Required(ErrorMessage = "required")]
     public string Surname { get; set; }
-
-    [Required(ErrorMessage = "required")]
-    [EmailAddress(ErrorMessage = "invalid")]
     public string Email { get; set; }
-
     public DateTime DateOfBirth { get; set; }
     public DateTime TeaTime { get; set; }
     public bool EmailConfirmed { get; set; }
--- Pixata.Blazor.Sample/Validators/PersonValidator.cs
+++ Pixata.Blazor.Sample/Validators/PersonValidator.cs
@@ -5,9 +5,19 @@ using Pixata.Blazor.Sample.Models;
 namespace Pixata.Blazor.Sample.Validators {
   public class PersonValidator : AbstractValidator<Person> {
     public PersonValidator() {
-      RuleFor(p => p.FirstName).NotEmpty().WithMessage("Required");
-      RuleFor(p => p.Surname).NotEmpty().WithMessage("Required");
-      RuleFor(p => p.DateOfBirth).GreaterThan(new DateTime(2000, 1, 1)).WithMessage("After 1st Jan 2000");
+      RuleFor(p => p.Title).NotEmpty().WithMessage("required");
+      RuleFor(p => p.FirstName).NotEmpty().WithMessage("required");
+      RuleFor(p => p.Surname).NotEmpty().WithMessage("required");
+      RuleFor(p => p.Email).Cascade(CascadeMode.Stop)
+        .NotEmpty().WithMessage("required")
+        .EmailAddress().WithMessage("invalid");
+      RuleFor(p => p.DateOfBirth).GreaterThan(new DateTime(2000, 1, 1)).WithMessage("after 1st Jan 2000");
+      RuleFor(p => p.TeaTime).GreaterThan(new DateTime(1, 1, 1, 12, 0, 0)).WithMessage("must be after noon");
+      RuleFor(p => p.Notes).Custom((notes, ctx) => {
+        if (notes.Contains("weird")) {
+          ctx.AddFailure("not nice to describe him as weird");
+        }
+      });
     }
   }
 }
