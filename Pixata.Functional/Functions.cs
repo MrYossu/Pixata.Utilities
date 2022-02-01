bb52388f80d17a5139fcba3f1d0f6f714b69f7d1
Avrohom Yisroel Silver <mryossu@hotmail.com>
2022-02-01 21:07:32 +0000

    Added the Pixata.Functional project

---
+++ Pixata.Functional/Functions.cs
@@ -0,0 +1,28 @@
+﻿namespace Pixata.Functional {
+  public static class Functions {
+    // https://stackoverflow.com/a/14071657/706346
+    public static T Identity<T>(T value) =>
+      value;
+
+    public static T0 Default<T0>() =>
+      default(T0);
+
+    public static T0 Default<T1, T0>(T1 value1) =>
+      default(T0);
+    /* Put as many overloads as you want */
+
+    /* Some other potential methods */
+    public static bool IsNull<T>(T entity) where T : class =>
+      entity == null;
+
+    public static bool IsNonNull<T>(T entity) where T : class =>
+      entity != null;
+
+    /* Put as many overloads for True and False as you want */
+    public static bool True<T>(T entity) =>
+      true;
+
+    public static bool False<T>(T entity) =>
+      false;
+  }
+}
