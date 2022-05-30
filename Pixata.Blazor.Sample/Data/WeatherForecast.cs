cac2702eca9b8bf885bf3ee17acccb5fe0240344
Avrohom Yisroel Silver <mryossu@hotmail.com>
2022-05-30 17:13:48 +0100

    Upgraded all projects to .NET6. Pixat.Funcational having code-gen problems, but all others working

--- Pixata.Blazor.Sample/Data/WeatherForecast.cs
+++ Pixata.Blazor.Sample/Data/WeatherForecast.cs
@@ -3,11 +3,9 @@ using System;
 namespace Pixata.Blazor.Sample.Data {
   public class WeatherForecast {
     public DateTime Date { get; set; }
-
     public int TemperatureC { get; set; }
-
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
+    public bool Rain { get; set; }
     public string Summary { get; set; }
   }
 }
