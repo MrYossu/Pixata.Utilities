dc8a139a1da95aebef7e777cefd90eeaf95c529f
Avrohom Yisroel Silver <mryossu@hotmail.com>
2024-10-14 21:47:17 +0100

    Added the grid column helpers to Pixata.Blazor.TelerikComponents, and upgraded it, and the sample project to .NET8

---
+++ Pixata.Blazor.TelerikComponents/Helpers/GridFilterOperators.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Telerik.Blazor.Components;
+using Telerik.DataSource;
+
+namespace Pixata.Blazor.TelerikComponents.Helpers;
+
+public static class GridFilterOperators {
+  public static List<FilterListOperator> StringOperators { get; set; } = [
+    new FilterListOperator { Operator = FilterOperator.Contains, Text = "contains" },
+    new FilterListOperator { Operator = FilterOperator.StartsWith, Text = "starts with" },
+    new FilterListOperator { Operator = FilterOperator.EndsWith, Text = "ends with" }
+  ];
+
+  public static List<FilterListOperator> NumericOperators { get; set; } = [
+    new FilterListOperator { Operator = FilterOperator.IsEqualTo, Text = "equals" },
+    new FilterListOperator { Operator = FilterOperator.IsLessThan, Text = "less than" },
+    new FilterListOperator { Operator = FilterOperator.IsGreaterThan, Text = "more than" }
+  ];
+
+  public static List<FilterListOperator> DateOperators { get; set; } = [
+    new FilterListOperator { Operator = FilterOperator.IsLessThan, Text = "before" },
+    new FilterListOperator { Operator = FilterOperator.IsEqualTo, Text = "on" },
+    new FilterListOperator { Operator = FilterOperator.IsGreaterThanOrEqualTo, Text = "on or after" }
+  ];
+}
---
+++ Pixata.Blazor.TelerikComponents/Helpers/TextGridColumn.cs
@@ -0,0 +1,11 @@
+﻿using Telerik.Blazor.Components;
+using Telerik.DataSource;
+
+namespace Pixata.Blazor.TelerikComponents.Helpers;
+
+public class TextGridColumn : GridColumn {
+  public TextGridColumn() {
+    FilterOperators = GridFilterOperators.StringOperators;
+    DefaultFilterOperator = FilterOperator.Contains;
+  }
+}
