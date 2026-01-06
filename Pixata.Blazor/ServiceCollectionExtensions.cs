4f2a39c17366918c927c9b28a8727296a3d58c87
Avrohom Yisroel Silver <mryossu@hotmail.com>
2026-01-06 20:53:09 +0000

    #8 Add an AddPixataBlazor() method

---
+++ Pixata.Blazor/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Pixata.Blazor.Extensions;
+using Pixata.Blazor.Notifications;
+
+namespace Pixata.Blazor;
+
+public static class ServiceCollectionExtensions {
+  public static IServiceCollection AddPixataBlazor(this IServiceCollection services) {
+    services.AddScoped<NotificationHelper>();
+    services.AddScoped<PasswordOptionsHelper>();
+    services.AddScoped<TemplateHelper>();
+    return services;
+  }
+}
