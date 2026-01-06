8f9ce3a72332e838ab2d40848cf3faeb28fa67f0
Avrohom Yisroel Silver <mryossu@hotmail.com>
2026-01-06 20:55:15 +0000

    #9 Add MessageBroker to Pixata.Blazor

--- Pixata.Blazor/ServiceCollectionExtensions.cs
+++ Pixata.Blazor/ServiceCollectionExtensions.cs
@@ -6,6 +6,7 @@ namespace Pixata.Blazor;
 
 public static class ServiceCollectionExtensions {
   public static IServiceCollection AddPixataBlazor(this IServiceCollection services) {
+    services.AddScoped<MessageBrokerInstance>();
     services.AddScoped<NotificationHelper>();
     services.AddScoped<PasswordOptionsHelper>();
     services.AddScoped<TemplateHelper>();
