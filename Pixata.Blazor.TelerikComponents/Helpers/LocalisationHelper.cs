596576c1c560eef8e563023840843d525ffeb3fb
Avrohom Yisroel Silver <mryossu@hotmail.com>
2026-01-12 21:28:33 +0000

    Added LocalisationHelper to Pixata.Blazor.TelerikComponents

---
+++ Pixata.Blazor.TelerikComponents/Helpers/LocalisationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Telerik.Blazor.Services;
+
+namespace Pixata.Blazor.TelerikComponents.Helpers;
+
+public class LocalisationHelper : ITelerikStringLocalizer {
+  private readonly ITelerikStringLocalizer _fallback = new TelerikStringLocalizer();
+
+  public readonly Dictionary<string, string> Values = new() {
+    { "Filter_SelectValue", "All" },
+    { "Grid_NoRecords", "Sorry, nothing matched your filters. Please widen your search criteria" },
+  };
+
+  public string this[string name] =>
+    Values.ContainsKey(name)
+      ? Values[name]
+      : _fallback[name];
+}
