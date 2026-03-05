b1f81e91afe08ce59f3c968223146dee94862289
Avrohom Yisroel Silver <mryossu@hotmail.com>
2026-03-05 16:16:13 +0000

    Added RequestLoggingMiddleware to Pixata.AspNetCore

---
+++ Pixata.AspNetCore/Extensions/RequestLoggingExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pixata.AspNetCore.Extensions;
+
+public static class RequestLoggingExtensions {
+  public static IServiceCollection AddRequestLogging(this IServiceCollection services, Action<RequestLoggingOptions> configure = null) {
+    services.AddOptions<RequestLoggingOptions>();
+    if (configure is not null) {
+      services.Configure(configure);
+    }
+    return services;
+  }
+
+  public static IApplicationBuilder UseRequestLogging(this IApplicationBuilder app) =>
+    app.UseMiddleware<RequestLoggingMiddleware>();
+}
