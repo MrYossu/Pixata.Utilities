6fb3cb8dc9eff95c38146ea08e14571e15b5d965
Avrohom Yisroel Silver <mryossu@hotmail.com>
2026-04-21 17:58:14 +0100

    Added two registrations needed for the DocumentTemplateHelper to Pixata.AspNetCore.ServiceCollectionExtensions

--- Pixata.AspNetCore/ServiceCollectionExtensions.cs
+++ Pixata.AspNetCore/ServiceCollectionExtensions.cs
@@ -1,11 +1,15 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.DependencyInjection;
 using Pixata.AspNetCore.Extensions;
+using Pixata.AspNetCore.Helpers;
 
 namespace Pixata.AspNetCore;
 
 public static class ServiceCollectionExtensions {
   public static IServiceCollection AddPixataAspNetCore<T>(this IServiceCollection services) {
+    services.AddScoped<HtmlRenderer>();
+    services.AddScoped<DocumentTemplateHelper>();
     services.AddValidatorsFromAssemblyContaining<T>();
     services.AddTransient<ValidationEndpointFilter>();
     return services;
