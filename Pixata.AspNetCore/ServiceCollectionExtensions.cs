2d05ebb2da60bf625655539b51e657e46d06417b
Avrohom Yisroel Silver <mryossu@hotmail.com>
2026-04-21 18:17:05 +0100

    Anopther dependency added to Pixata.AspNetCore.AddPixataAspNetCore

--- Pixata.AspNetCore/ServiceCollectionExtensions.cs
+++ Pixata.AspNetCore/ServiceCollectionExtensions.cs
@@ -3,11 +3,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.DependencyInjection;
 using Pixata.AspNetCore.Extensions;
 using Pixata.AspNetCore.Helpers;
+using WkHtmlToPdfDotNet;
+using WkHtmlToPdfDotNet.Contracts;
 
 namespace Pixata.AspNetCore;
 
 public static class ServiceCollectionExtensions {
   public static IServiceCollection AddPixataAspNetCore<T>(this IServiceCollection services) {
+    services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
     services.AddScoped<HtmlRenderer>();
     services.AddScoped<DocumentTemplateHelper>();
     services.AddValidatorsFromAssemblyContaining<T>();
