088b12b6d26bc7657b484f1716648a2d5db9e175
Avrohom Yisroel Silver <mryossu@hotmail.com>
2026-05-31 16:27:12 +0100

    Pixata.Extensions v2.7.0

--- Pixata.AspNetCore/Auditing/Models/Audit.cs
+++
@@ -1,2 +0,0 @@
-// Audit and AuditOperation have been moved to Pixata.Extensions.Auditing.Models.
-// This file is retained as a global alias so existing code in this project continues to compile.
--- Pixata.AspNetCore/Auditing/Models/AuditOperation.cs
+++
@@ -1,2 +0,0 @@
-// AuditOperation has been moved to Pixata.Extensions.Auditing.Models.
-// This file is retained as a placeholder; the global alias is declared in GlobalAuditingUsings.cs.
