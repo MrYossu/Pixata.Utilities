479f87db7d8b670f0f0388aa34da89c5b2762b5f
Avrohom Yisroel Silver <mryossu@hotmail.com>
2026-06-01 18:13:49 +0100

    Added the RemoveTime method to TelerikFilterHelper

--- Pixata.Blazor.TelerikComponents/Helpers/GridFilterOperators.cs
+++ Pixata.Blazor.TelerikComponents/Helpers/GridFilterOperators.cs
@@ -6,20 +6,20 @@ namespace Pixata.Blazor.TelerikComponents.Helpers;
 
 public static class GridFilterOperators {
   public static List<FilterListOperator> StringOperators { get; set; } = [
-    new FilterListOperator { Operator = FilterOperator.Contains, Text = "contains" },
-    new FilterListOperator { Operator = FilterOperator.StartsWith, Text = "starts with" },
-    new FilterListOperator { Operator = FilterOperator.EndsWith, Text = "ends with" }
+    new() { Operator = FilterOperator.Contains, Text = "contains" },
+    new() { Operator = FilterOperator.StartsWith, Text = "starts with" },
+    new() { Operator = FilterOperator.EndsWith, Text = "ends with" }
   ];
 
   public static List<FilterListOperator> NumericOperators { get; set; } = [
-    new FilterListOperator { Operator = FilterOperator.IsEqualTo, Text = "equals" },
-    new FilterListOperator { Operator = FilterOperator.IsLessThan, Text = "less than" },
-    new FilterListOperator { Operator = FilterOperator.IsGreaterThan, Text = "more than" }
+    new() { Operator = FilterOperator.IsEqualTo, Text = "equals" },
+    new() { Operator = FilterOperator.IsLessThan, Text = "less than" },
+    new() { Operator = FilterOperator.IsGreaterThan, Text = "more than" }
   ];
 
   public static List<FilterListOperator> DateOperators { get; set; } = [
-    new FilterListOperator { Operator = FilterOperator.IsLessThan, Text = "before" },
-    new FilterListOperator { Operator = FilterOperator.IsEqualTo, Text = "on" },
-    new FilterListOperator { Operator = FilterOperator.IsGreaterThanOrEqualTo, Text = "on or after" }
+    new() { Operator = FilterOperator.IsLessThan, Text = "before" },
+    new() { Operator = FilterOperator.IsEqualTo, Text = "on" },
+    new() { Operator = FilterOperator.IsGreaterThanOrEqualTo, Text = "on or after" }
   ];
 }
--- Pixata.Blazor.TelerikComponents/Helpers/TelerikFilterHelper.cs
+++ Pixata.Blazor.TelerikComponents/Helpers/TelerikFilterHelper.cs
@@ -1,22 +1,24 @@
-﻿using Telerik.DataSource;
+﻿using System;
+using Telerik.Blazor.Components;
+using Telerik.DataSource;
 
 namespace Pixata.Blazor.TelerikComponents.Helpers;
 
 public static class TelerikFilterHelper {
   public static CompositeFilterDescriptor CreateSimple(string member, FilterOperator op, object value) =>
     new() {
-      FilterDescriptors = new FilterDescriptorCollection {
+      FilterDescriptors = [
         new FilterDescriptor {
           Member = member,
           Operator = op,
           Value = value,
           MemberType = value.GetType()
         }
-      }
+      ]
     };
 
   public static CompositeFilterDescriptor CreateAnd(string member, params OperatorValue[] ovs) {
-    FilterDescriptorCollection filters = new();
+    FilterDescriptorCollection filters = [];
     foreach (OperatorValue ov in ovs) {
       filters.Add(new FilterDescriptor(member, ov.Op, ov.Value) { MemberType = ov.Value.GetType() });
     }
@@ -27,7 +29,7 @@ public static class TelerikFilterHelper {
   }
 
   public static CompositeFilterDescriptor CreateOr(string member, params OperatorValue[] ovs) {
-    FilterDescriptorCollection filters = new();
+    FilterDescriptorCollection filters = [];
     foreach (OperatorValue ov in ovs) {
       filters.Add(new FilterDescriptor(member, ov.Op, ov.Value) { MemberType = ov.Value.GetType() });
     }
@@ -35,6 +37,47 @@ public static class TelerikFilterHelper {
       LogicalOperator = FilterCompositionLogicalOperator.Or,
       FilterDescriptors = filters
     };
+  }
+
+  /// <summary>
+  /// For each named DateTime member, modifies an IsEqualTo filter so that filtering by date works even when the value has a non-zero time part
+  /// </summary>
+  /// <param name="args">The GridReadEventArgs object passed to OnRead</param>
+  /// <param name="dateMembers">The name(s) of the DateTime member(s) to adjust</param>
+  public static void RemoveTime(GridReadEventArgs args, params string[] dateMembers) {
+    foreach (string member in dateMembers) {
+      DateTime? filterDate = null;
+      foreach (IFilterDescriptor filter in args.Request.Filters) {
+        if (filter is CompositeFilterDescriptor cfd) {
+          foreach (IFilterDescriptor inner in cfd.FilterDescriptors) {
+            if (inner is FilterDescriptor fd) {
+              CheckAndFix(fd, member, ref filterDate);
+            }
+          }
+        } else if (filter is FilterDescriptor fd) {
+          CheckAndFix(fd, member, ref filterDate);
+        }
+      }
+      if (filterDate is not null) {
+        args.Request.Filters.Add(new CompositeFilterDescriptor {
+          FilterDescriptors = [
+            new FilterDescriptor {
+              Member = member,
+              MemberType = typeof(DateTime),
+              Operator = FilterOperator.IsLessThan,
+              Value = filterDate.Value.AddDays(1)
+            }
+          ]
+        });
+      }
+    }
+  }
+
+  private static void CheckAndFix(FilterDescriptor fd, string member, ref DateTime? filterDate) {
+    if (fd.Member == member && fd is { Operator: FilterOperator.IsEqualTo, Value: not null }) {
+      fd.Operator = FilterOperator.IsGreaterThanOrEqualTo;
+      filterDate = (DateTime)fd.Value;
+    }
   }
 }
 
